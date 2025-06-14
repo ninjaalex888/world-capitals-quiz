34e61f5a623c8890376ec5a209bdd7533e295c4b
Alex Sheehan <alex.sheehan95@gmail.com>
2025-06-14 09:24:18 -0400

    Add files via upload

--- quiz.js
+++ quiz.js
@@ -1,3 +1,11 @@
+
+// --- Utility: Shuffle array in place ---
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
 const data = [
   {country: "Japan", capital: "Tokyo", cities: ['Osaka', 'Kyoto', 'Nagoya', 'Sapporo']},
   {country: "Canada", capital: "Ottawa", cities: ['Toronto', 'Vancouver', 'Montreal', 'Calgary']},
